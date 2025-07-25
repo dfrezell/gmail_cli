db37d6f8d804c5b522ad6a442985688a3b52eaea
Andrew Frezell <drew@frezell.com>
2025-07-24 23:09:04 -0400

    user newer email api in python

--- gmail_sender.py
+++ gmail_sender.py
@@ -1,20 +1,16 @@
 import base64
 import mimetypes
 import os
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
+from email.message import EmailMessage
 from gmail_auth import authenticate_gmail
 
 def create_message(sender, to, subject, message_html, message_text, attachments=None):
-    message = MIMEMultipart("mixed" if attachments else "alternative")
-    message['to'] = to
-    message['from'] = sender
-    message['subject'] = subject
-    
-    message.attach(MIMEText(message_text, 'plain'))
-    message.attach(MIMEText(message_html, 'html'))
+    message = EmailMessage()
+    message['From'] = sender
+    message['To'] = to
+    message['Subject'] = subject
+    message.set_content(message_text, subtype='plain')
+    message.add_alternative(message_html, subtype='html')
     
     if attachments:
         for file_path in attachments:
@@ -26,14 +22,12 @@ def create_message(sender, to, subject, message_html, message_text, attachments=
                 main_type, sub_type = content_type.split('/', 1)
                 
                 with open(file_path, 'rb') as fp:
-                    attachment = MIMEBase(main_type, sub_type)
-                    attachment.set_payload(fp.read())
-                    encoders.encode_base64(attachment)
-                    attachment.add_header(
-                        'Content-Disposition',
-                        f'attachment; filename="{os.path.basename(file_path)}"'
+                    message.add_attachment(
+                        fp.read(),
+                        maintype=main_type,
+                        subtype=sub_type,
+                        filename=os.path.basename(file_path)
                     )
-                    message.attach(attachment)
     
     raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
     return {'raw': raw_message}
